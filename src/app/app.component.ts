9a861e0e63882a6d3984952882377ac4f7446175
tkovalev <tkovalev@satel.org>
2021-06-10 19:58:49 +0300

    feat: add fluid design system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import '@engie-group/fluid-design-system';
+import '@engie-group/fluid-design-system/lib/auto-init';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() { }
 }
